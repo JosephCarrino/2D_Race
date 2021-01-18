417443a69c2b8ee96d36a7f98b3495a6dffad033
JosephCarrino <77633298+JosephCarrino@users.noreply.github.com>
2021-01-18 18:26:48 +0100

    Add files via upload

---
+++ Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/DoStrada.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/DoStrada.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Fuel.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Fuel.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Funzioni.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Funzioni.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Gas.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Gas.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Liste.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Liste.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Macchina.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Macchina.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Menu.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Menu.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Ostacolo1.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Ostacolo1.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Ostacolo2.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Ostacolo2.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Ostacolo3.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Ostacolo3.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Pausa.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Pausa.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Score.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Score.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/Title.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/Title.cpp.obj"
+  "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/main.cpp" "C:/Users/josep/Desktop/Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Progetto_2D_Race/2D_Race/cmake-build-debug/CMakeFiles/2D_Race.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "2D_Race.exe"
+  "2D_Race.exe.manifest"
+  "2D_Race.pdb"
+  "CMakeFiles/2D_Race.dir/DoStrada.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Fuel.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Funzioni.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Gas.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Liste.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Macchina.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Menu.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Ostacolo1.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Ostacolo2.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Ostacolo3.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Pausa.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Score.cpp.obj"
+  "CMakeFiles/2D_Race.dir/Title.cpp.obj"
+  "CMakeFiles/2D_Race.dir/main.cpp.obj"
+  "lib2D_Race.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2D_Race.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
